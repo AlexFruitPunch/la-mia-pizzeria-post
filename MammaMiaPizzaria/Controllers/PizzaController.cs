aa9b063085cc0762c90a76a9c9e9026d370aa9ec
alexpagani <alexpaganibr@gmail.com>
2022-05-12 16:25:37 +0200

    Add project files.

---
+++ MammaMiaPizzaria/Controllers/PizzaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MammaMiaPizzaria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]                                                   //dichiariamo che questo metodo vuole fare una GET
+        [Route("/ListinoPizze")]                                    //Dichiariamo qual'è il path da seguire per questo metodo
+        public IActionResult ListinoPizze()
+        {
+            return View("Listino-Pizze");
+        }
+    }
+}
---
+++ MammaMiaPizzaria/Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace MammaMiaPizzaria.Models
+{
+    public class Pizza
+    {
+        public string? Nome { get; set; }
+        public string? Ingredienti { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza() 
+        {
+            
+        }
+
+        public Pizza(string Nome, string Ingredienti, double Prezzo)
+        {
+            this.Nome = Nome;
+            this.Ingredienti = Ingredienti;
+            this.Prezzo = Prezzo;
+        }
+    }
+}

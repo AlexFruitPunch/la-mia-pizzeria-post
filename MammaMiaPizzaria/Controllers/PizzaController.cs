84cc33810c8fd26404b9d49af4663ff048013927
alexpagani <alexpaganibr@gmail.com>
2022-05-13 11:14:02 +0200

    modifica nome view

--- MammaMiaPizzaria/Controllers/PizzaController.cs
+++ MammaMiaPizzaria/Controllers/PizzaController.cs
@@ -4,11 +4,10 @@ namespace MammaMiaPizzaria.Controllers
 {
     public class PizzaController : Controller
     {
-        [HttpGet]                                                   //dichiariamo che questo metodo vuole fare una GET
-        [Route("/ListinoPizze")]                                    //Dichiariamo qual'è il path da seguire per questo metodo
+        [HttpGet]                                                   //dichiariamo che questo metodo vuole fare una GET                                    //Dichiariamo qual'è il path da seguire per questo metodo
         public IActionResult ListinoPizze()
         {
-            return View("Listino-Pizze");
+            return View("ListinoPizze");
         }
     }
 }
